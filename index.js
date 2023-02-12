1c45c078568e3c77bfd96cffb44ffc481aa1f807
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-02-12 21:19:55 +0900

    update

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ var prismarine_chunk_2 = require("prismarine-chunk");
 var prismarine_registry_1 = require("prismarine-registry");
 var loader = require('prismarine-block');
 var DataRegistries = /** @class */ (function () {
-    function DataRegistries(version) {
-        this.minecraft = (0, minecraft_data_1["default"])(version);
-        var registry = (0, prismarine_registry_1["default"])(version);
-        // any hack: (typeof BedrockChunk | typeof PCChunk)
-        var chunkType = (0, prismarine_chunk_2["default"])(version);
-        if (typeof chunkType == typeof prismarine_chunk_1.PCChunk) {
-            throw Error("only accept bedrock chunk type");
-        }
-        var bedrockChunkType = chunkType;
-        this.chunk = bedrockChunkType;
-        this.block = loader(registry);
-    }
-    return DataRegistries;
+	function DataRegistries(version) {
+		this.minecraft = minecraft_data_1(version);
+		var registry = prismarine_registry_1(version);
+		// any hack: (typeof BedrockChunk | typeof PCChunk)
+		var chunkType = prismarine_chunk_2(version);
+		if (typeof chunkType == typeof prismarine_chunk_1.PCChunk) {
+			throw Error("only accept bedrock chunk type");
+		}
+		var bedrockChunkType = chunkType;
+		this.chunk = bedrockChunkType;
+		this.block = loader(registry);
+	}
+	return DataRegistries;
 }());
 exports.DataRegistries = DataRegistries;
 exports.latestRegistries = new DataRegistries("bedrock_1.19.60");
 function get(version) {
-    return new DataRegistries(version);
+	return new DataRegistries(version);
 }
 exports.get = get;
