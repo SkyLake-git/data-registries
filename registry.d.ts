6fc9248b453e2d243806c1a345813899889bccd2
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-02-12 10:02:36 +0900

    declare update

--- registry.d.ts
+++ registry.d.ts
@@ -2,15 +2,18 @@ import { Block } from 'prismarine-block'
 import { BedrockChunk } from 'prismarine-chunk'
 import { IndexedData } from 'minecraft-data'
 
-export class DataRegistries {
+declare module 'data-registries' {
+	export class DataRegistries {
 
-	minecraft: IndexedData
-	chunk: typeof BedrockChunk
-	block: typeof Block
+		minecraft: IndexedData
+		chunk: typeof BedrockChunk
+		block: typeof Block
 
-	constructor(version: string)
+		constructor(version: string)
+	}
+
+	export let latestRegistries: DataRegistries
+
+	export function get(version: string): DataRegistries
 }
 
-export let latestRegistries: DataRegistries
-
-export function get(version: string): DataRegistries
